b4ad61e9b71d168e3ba2dc777c966bc247e0c8eb
Mahmoud Attia <51999205+mamoda@users.noreply.github.com>
2026-03-21 14:57:53 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -523,7 +523,6 @@ return `
             
             body {
                 font-family: 'Tahoma', 'Arial', 'Segoe UI', sans-serif;
-                background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
                 padding: 20px;
                 min-height: 100vh;
             }
